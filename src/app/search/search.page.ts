067f1d257ad49d41981a4dcf346ca622ff61bf6f
josevergelin <75032564+josevergelin@users.noreply.github.com>
2021-06-05 22:44:47 -0500

    avance

--- src/app/search/search.page.ts
+++ src/app/search/search.page.ts
@@ -7,6 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchPage implements OnInit {
 
+  huecas = [{
+    lugar: "Quito",
+    title: "El Santo Hornado",
+    description: "Descripción del Santo Hornado",
+    imageUrl: "images/santo-hornado.jpg"
+  },
+    {
+      lugar: "Guayaquil",
+      title: "El Cafe de Tere",
+      description: "Descripción del Cafe de Tere",
+      imageUrl: "images/cafe-tere.jpg"
+    },
+    {
+      lugar: "Riobamba",
+      title: "El Cafe de Riobamba",
+      description: "Descripción del Cafe de Riobamba",
+      imageUrl: "images/cafe-tere.jpg"
+    },
+    {
+      lugar: "Quito",
+      title: "Los Hot Dog de la Gonsalez",
+      description: "Descripción de los Hot Dog de la Gonsalez",
+      imageUrl: "images/hotdog-gonzalez.jpg"
+    }]
+
   constructor() { }
 
   ngOnInit() {
